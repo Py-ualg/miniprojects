7bf33fb12f24a1518018886bb8cdc09e0132ae44
palec87 <david@stanka.de>
2024-04-15 19:16:16 +0100

    docs, comments, fixed labels and centroids display

--- streamlit_dashboard/app.py
+++ streamlit_dashboard/app.py
@@ -2,18 +2,22 @@ import streamlit as st
 from utils import runModel, plotClusters
 import pandas as pd
 
-df = pd.read_csv('data/iris.csv')
-kmeans, X_train, X_test, y_train, y_test = runModel(df)
+__author__ = "David Palecek"
+__email__ = "david@stanka.de"
 
+df = pd.read_csv('data/iris.csv')  # load the iris dataset
+kmeans, X_train, X_test, y_train, y_test = runModel(df)  # run the model
+labels = ['Virginica', 'Setosa', 'Versicolor']  # labels for the clusters
 
 
 def main():
     st.title('Dashboard from Iris dataset.')
 
-    menu = ["Intro analysis", "Model", "Prediction"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    menu = ["Introduction", "Model", "Prediction"]    # menu options
+    choice = st.sidebar.selectbox("Menu", menu)         # sidebar to select from the menu
 
-    if choice == "Intro analysis":
+    # introduction to the iris dataset
+    if choice == "Introduction":
         st.subheader("Iris dataset")
         # write intro text to iris dataset
         st.write("""
@@ -25,17 +29,18 @@ def main():
         # dispalay dataframe
         st.dataframe(df.head())
 
+    # plot the clusters using the plot_clusters function
     elif choice == "Model":
         st.subheader("KMeans Clustering Model")
-        # plot the clusters using the plot_clusters function
         # select of columns to plot using streamlit
-        xcol = st.selectbox("Select the columns to plot on X", df.columns)
-        ycol = st.selectbox("Select the columns to plot on Y", df.columns)
+        xcol = st.selectbox("Select the columns to plot on X", df.columns[:-1])
+        ycol = st.selectbox("Select the columns to plot on Y", df.columns[:-1])
         
 
         fig = plotClusters(kmeans, X_train, y_train, xcol, ycol)
         st.pyplot(fig)
-        
+    
+    # predictions of user's input
     elif choice == "Prediction":
         st.subheader("Input your measeurements to predict the cluster.")
         # create a form to input the measurements
@@ -67,7 +72,11 @@ def main():
         prediction = kmeans.predict(input_data)
 
         # display the cluster in bigger font
-        st.subheader(f"The cluster is {prediction[0]}")
+        st.subheader(f"The cluster is {labels[prediction[0]]}")
+
+        # you can extend this for example with
+        # displaying the probabilities of the cluster using kmeans.predict_proba(input_data)
+        # visualizing the input values against training data using the plotClusters function
 
 if __name__ == "__main__":
     main()
--- streamlit_dashboard/utils.py
+++ streamlit_dashboard/utils.py
@@ -6,7 +6,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def runModel(dataset):
+def runModel(dataset: pd.DataFrame) -> tuple:
+    """KMeans clustering model
+
+    Args:
+        dataset (pd.DataFrame): dataset to run the model on
+
+    Returns:
+        tuple: model, x_train, x_test, y_train, y_test
+    """
     X = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, -1].values
 
@@ -18,23 +26,42 @@ def runModel(dataset):
     return kmeans, X_train, X_test, y_train, y_test
 
 
-def plotClusters(kmeans, X_train:np.ndarray, y_train: np.ndarray, xcol, ycol):
-    #convert X_train to a dataframe
-    X_train = pd.DataFrame(X_train, columns=['sepal.length', 'sepal.width', 'petal.length', 'petal.width'])
-    fig, ax = plt.subplots(1, 2, figsize=(10, 8), sharey=True)
+def plotClusters(kmeans, X_train:np.ndarray, y_train: np.ndarray, xcol: str, ycol: str) -> plt.figure:
+    """ plot the clusters
 
+    Args:
+        kmeans (kmeans Model): result of the kmeans model
+        X_train (np.ndarray): training data
+        y_train (np.ndarray): training labels
+        xcol (str): column to plot on x-axis
+        ycol (str): column to plot on y-axis
+
+    Returns:
+        plt.figure: resulting plot
+    """
+    #convert X_train to a dataframe
+    columns = ['sepal.length', 'sepal.width', 'petal.length', 'petal.width']
+    X_train = pd.DataFrame(X_train, columns=columns)
+    fig, ax = plt.subplots(1, 2, figsize=(10, 8), sharey=True)
+    labels = ['Virginica', 'Setosa', 'Versicolor']
+    # convert kmeans.labels_ to list of strings
+    kmeans.labels_ = [labels[i] for i in kmeans.labels_]
     # put ax as attribute to the scatterplot function
     sns.scatterplot(x=xcol, y=ycol,
-                    data=X_train, ax=ax[0])
-    sns.scatterplot(x=xcol, y=ycol,
                     data=X_train, hue=kmeans.labels_,
-                    palette='viridis', ax=ax[0])
-    sns.scatterplot(x=kmeans.cluster_centers_[:, 0],
-                    y=kmeans.cluster_centers_[:, 1],
+                    hue_order=labels,
+                    palette='Set2', ax=ax[0],
+                    )
+
+    sns.scatterplot(x=kmeans.cluster_centers_[:, columns.index(xcol)],
+                    y=kmeans.cluster_centers_[:, columns.index(ycol)],
                     s=100, color='red', label='Centroids', ax=ax[0])
+    ax[0].set_title('Training data predictions')
     
     # plot the ground truth clusters
     sns.scatterplot(x=xcol, y=ycol,
                     data=X_train, hue=y_train,
-                    palette='viridis', ax=ax[1])
+                    hue_order=labels,
+                    palette='Set2', ax=ax[1])
+    ax[1].set_title('Ground truth clusters')
     return fig
